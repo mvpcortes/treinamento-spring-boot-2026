08ffeca4417c1bffd2580389fab529385339224f
Marcos Vinicius Policarpo Côrtes <marcoscortes@id.uff.br>
2026-03-23 13:28:08 -0300

    por mvpcortes: ajuste dos nomes

--- ap2/src/main/java/br/uff/sti/ap2/Ap21Application.java
+++ ap2/src/main/java/br/uff/sti/ap2/Ap21Application.java
@@ -3,11 +3,11 @@ package br.uff.sti.ap2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-public class Ap2Application {
+@SpringBootApplication
+public class Ap21Application {
 
 	public static void main(String[] args) {
-		try(var context = SpringApplication.run(Ap2Application.class, args)){
+		try(var context = SpringApplication.run(Ap21Application.class, args)){
 			context.getBean(AloMundoComponent.class).aloMundo("jupiter");
 		}
 	}
--- ap2/src/main/java/br/uff/sti/ap2/Ap22Application.java
+++ ap2/src/main/java/br/uff/sti/ap2/Ap22Application.java
@@ -3,7 +3,7 @@ package br.uff.sti.ap2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Ap22Application {
 
 	public static void main(String[] args) {
